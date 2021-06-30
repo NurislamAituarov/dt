9f7d9d02a57a05c358b963469c744f132e0fa3f7
Nurislam <nurislam-1994@mail.ru>
2021-06-30 16:08:23 +0600

    tip git

---
+++ dinamiTipizac.js
@@ -0,0 +1,66 @@
+const wrapper = document.querySelector('.sidebar');
+const btns = document.querySelectorAll('button');
+const ahrev = document.querySelector('a');
+const kvd = document.querySelectorAll('.kvadr')
+function hideContent(){
+    kvd.forEach(function(item){
+        item.style.display = 'none'
+    })
+    btns.forEach(function(item){
+        item.classList.remove('sty')
+    })
+}
+function showContent(i=0){
+    kvd[i].style.display = 'block';
+    btns[i].classList.add('sty')
+}
+hideContent()
+showContent()
+wrapper.addEventListener('click',function(event){
+    const a = event.target;
+    if(a && a.classList.contains('btn')){
+        btns.forEach(function(item,i){
+            if(item == a){
+                hideContent()
+                showContent(i)
+            }
+        })
+    }
+})
+
+// console.log(wrapper)/////Обертка кнопок
+// wrapper.addEventListener('click', function(event){
+//     event.target.parentElement.previousElementSibling.style.borderRadius = '20px'    
+//     // wrapper.previousElementSibling.style.backgroundColor = 'red'
+// })
+// const button = document.createElement('button');
+// button.classList.add('btn')
+// // sidebar.append(button)
+// btns[3].replaceWith(button)
+
+// btns.forEach(function(item,i){
+//     item.innerHTML = ''
+//     item.innerHTML = `${i+1} кнопка`
+//     item.style.color = 'black'
+// })
+// btns[0].addEventListener('click',function(){
+//     ahrev.innerHTML = ''
+//     ahrev.innerHTML = `<a href="https://www.youtube.com/watch?v=Z7DvBFU_xRI" target="_blank">новая ссылка</a>`
+//     btns[1].addEventListener('click', function(){
+//         ahrev.innerHTML = `<a href="https://www.youtube.com/" target="_blank">ссылка на ютуб</a>`
+//     })
+// })
+
+// wrapper.addEventListener('click',function(event){
+//     // console.dir(event.target)
+//     if(event.target.tagName == 'BUTTON'){
+//         event.target.style.cssText = 'border-radius: 50px;color:black;'
+//     }else{
+//         event.target.style.cssText = 'background-color:red;border-radius: 50px;'
+//     }   
+// })
+// btns[3].innerHTML = '';
+// btns[3].innerHTML = `<a href="https://www.youtube.com/" target="_blank">last</a>`
+// btns[3].addEventListener('click',function(event){
+//     event.preventDefault()
+// })
